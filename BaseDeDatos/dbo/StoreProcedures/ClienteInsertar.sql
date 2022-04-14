5f0d4aed8ffb28111111ed1bf46b5d9ca0eb2b7c
StevDev <brayansancho38@gmail.com>
2022-04-14 14:55:40 -0600

    Tablas y Procedimientos almacenados creados

---
+++ BaseDeDatos/dbo/StoreProcedures/ClienteInsertar.sql
@@ -0,0 +1,77 @@
+﻿CREATE PROCEDURE [dbo].[ClienteInsertar]
+	@IdCliente INT,
+	@Identificacion VARCHAR (128),
+	@IdTipoIdentificacion INT,
+	@Nombre VARCHAR (128),
+	@PrimerApellido VARCHAR (128),
+	@SegundoApellido VARCHAR (128),
+	@FechaNacimiento DATETIME,
+	@Nacionalidad INT,
+	@FechaDefuncion DATETIME,
+	@Genero CHAR (1),
+	@NombreApellidosPadre VARCHAR (200),
+	@NombreApellidosMadre VARCHAR (200),
+	@Pasaporte VARCHAR (50),
+	@CuentaIBAN VARCHAR(50),
+	@CorreoNotifica VARCHAR(128)
+AS BEGIN
+
+SET NOCOUNT ON
+
+	BEGIN TRANSACTION TRASA
+
+	BEGIN TRY
+
+	INSERT INTO dbo.Cliente
+	(
+		IdCliente,
+		Identificacion,
+		IdTipoIdentificacion,
+		Nombre,
+		PrimerApellido,
+		SegundoApellido,
+		FechaNacimiento,
+		Nacionalidad,
+		FechaDefuncion,
+		Genero,
+		NombreApellidosPadre,
+		NombreApellidosMadre,
+		Pasaporte,
+		CuentaIBAN,
+		CorreoNotifica
+	)
+	VALUES
+	(
+		@IdCliente,
+		@Identificacion,
+		@IdTipoIdentificacion,
+		@Nombre,
+		@PrimerApellido,
+		@SegundoApellido,
+		@FechaNacimiento,
+		@Nacionalidad,
+		@FechaDefuncion,
+		@Genero,
+		@NombreApellidosPadre,
+		@NombreApellidosMadre,
+		@Pasaporte,
+		@CuentaIBAN,
+		@CorreoNotifica
+	)
+
+	COMMIT TRANSACTION TRASA
+	SELECT 0 As CodeError, '' AS MsgError
+
+	END TRY
+
+	BEGIN CATCH
+
+		SELECT
+			ERROR_NUMBER() AS CodeError,
+			ERROR_MESSAGE() AS MsgError
+
+		ROLLBACK TRANSACTION TRASA
+
+	END CATCH
+
+END
---
+++ BaseDeDatos/dbo/StoreProcedures/ClienteObtener.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[ClienteObtener]
+		@IdCliente INT = NULL
+AS
+Begin
+	SET NOCOUNT ON
+
+	SELECT
+		IdCliente,
+		Identificacion,
+		IdTipoIdentificacion,
+		Nombre,
+		PrimerApellido,
+		SegundoApellido,
+		FechaNacimiento,
+		Nacionalidad,
+		FechaDefuncion,
+		Genero,
+		NombreApellidosPadre,
+		NombreApellidosMadre,
+		Pasaporte,
+		CuentaIBAN,
+		CorreoNotifica
+	FROM dbo.Cliente
+	WHERE (@IdCliente IS NULL OR IdCliente=@IdCliente)
+End
