b3a0c3a9248f723a0c3f1bfae8746810d2e9c9d5
Bryan Peraza <Bryan Peraza@DESKTOP-FLMNRN9>
2022-04-14 21:53:20 -0600

    StoreProcedure(Servicio)

---
+++ BaseDeDatos/dbo/StoreProcedures/ServicioObtener.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[ServicioObtener]
+		@IdServicio INT = NULL
+AS
+Begin
+	SET NOCOUNT ON
+
+	SELECT
+		IdServicio,
+		NombreServicio,
+		PlazoEntrega,
+		CostoServicio,
+		Estado,
+		CuentaContable
+
+	FROM dbo.Servicio
+	WHERE (@IdServicio IS NULL OR IdServicio=@IdServicio)
+End
