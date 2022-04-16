fb994ea484ac6b513e082dadbebd9af02d0609c3
Bryan Peraza <Bryan Peraza@DESKTOP-FLMNRN9>
2022-04-15 23:40:32 -0600

    Service,Grid,Edit,TS,App

---
+++ WBL/ServicioService.cs
@@ -0,0 +1,123 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public interface IServicioService
+    {
+        Task<DBEntity> Create(ServicioEntity entity);
+        Task<DBEntity> Delete(ServicioEntity entity);
+        Task<IEnumerable<ServicioEntity>> Get();
+        Task<ServicioEntity> GetByID(ServicioEntity entity);
+        Task<DBEntity> Update(ServicioEntity entity);
+    }
+
+    public class ServicioService : IServicioService
+    {
+        private readonly IDataAccess sql;
+
+        public ServicioService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+
+        #region Metodos CRUD
+
+        public async Task<IEnumerable<ServicioEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<ServicioEntity>("dbo.ServicioObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //Medoto de obtener por Id
+
+        public async Task<ServicioEntity> GetByID(ServicioEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<ServicioEntity>("dbo.ServicioObtener", new { entity.IdServicio });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Create(ServicioEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ServicioInsertar",
+                new
+                {
+                    entity.NombreServicio,
+                    entity.PlazoEntrega,
+                    entity.CostoServicio,
+                    entity.Estado,
+                    entity.CuentaContable
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(ServicioEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ServicioActualizar", new
+                {
+                    entity.IdServicio,
+                    entity.NombreServicio,
+                    entity.PlazoEntrega,
+                    entity.CostoServicio,
+                    entity.Estado,
+                    entity.CuentaContable
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Delete(ServicioEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("dbo.ServicioEliminar", new { entity.IdServicio });
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ WebApp/wwwroot/App/Servicio/Grid.js
@@ -0,0 +1,18 @@
+"use strict";
+var ServicioGrid;
+(function (ServicioGrid) {
+    if (MensajeApp != "") {
+        Toast.fire({ icon: "success", title: MensajeApp });
+    }
+    function OnClickEliminar(id) {
+        ComfirmAlert("Desea eliminar el registro eliminado?", "Eliminar", "warning", '#3085d6', '#d33')
+            .then(function (result) {
+            if (result.isConfirmed) {
+                window.location.href = "Servicio/Grid?handler=Eliminar&id=" + id;
+            }
+        });
+    }
+    ServicioGrid.OnClickEliminar = OnClickEliminar;
+    $("#GridView").DataTable();
+})(ServicioGrid || (ServicioGrid = {}));
+//# sourceMappingURL=Grid.js.map
