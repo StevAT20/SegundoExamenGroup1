f62e609d5b27c272e2b4b1304250c5517565f2bf
JennyLB12 <jennylb2022@gmail.com>
2022-04-16 19:08:34 -0600

    StoreProcedures

---
+++ BaseDeDatos/dbo/StoreProcedures/SolicitudObtener.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[SolicitudObtener]
+	@IdSolicitud INT = NULL
+AS 
+BEGIN
+  SET NOCOUNT ON
+
+  SELECT
+       IdSolicitud
+	   ,Cantidad
+	   ,Monto
+	   ,FechaEntrega
+	   ,UsuarioEntrega
+	   ,Observaciones
+  FROM 
+       dbo.Solicitud
+  WHERE
+      (@IdSolicitud IS NULL OR IdSolicitud=@IdSolicitud)
+
+END
